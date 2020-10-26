08033458c95f64d72daa1a76cfc24d143d528fb4
EC2 Default User <ec2-user@ip-172-31-86-149.ec2.internal>
2020-10-26 07:55:56 +0000

    [Add] 投稿一覧画面作成、投稿詳細画面作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,27 @@
 class BooksController < ApplicationController
-  
-  def new
-  end
-
-  def create
-  end
 
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
   end
   
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
