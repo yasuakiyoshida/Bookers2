26cb40c06f2394c76a78405e13b469239c59741c
EC2 Default User <ec2-user@ip-172-31-86-149.ec2.internal>
2020-10-27 01:55:12 +0000

    [Add] プロフィール編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 end
