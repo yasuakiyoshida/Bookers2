acda97e3d87924e64a40ae65396c5168daa384ba
EC2 Default User <ec2-user@ip-172-31-86-149.ec2.internal>
2020-10-28 04:44:32 +0000

    [Add] ヘッダーのデザイン、マイページ作成、aboutページ作成、ログイン後のページ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-      before_action :authenticate_user!,except: [:top]
-      before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,except: [:top, :index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+      
+  def after_sign_in_path_for(resources)
+    user_path(current_user.id)
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources "homes", path: 'home', except: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    collection do
+      get 'about'
+    end
+  end
 end
